25d2c65efcc9e734ea47fc66a9feaf829610ae85
Bruno Cardoso <bruno.alvescardoso@hotmail.com>
2022-07-01 13:41:35 -0300

    exemplos aula3

---
+++ comparacoes.js
@@ -0,0 +1,12 @@
+// comparação implicita
+
+const numero = 7;
+const texto = "7";
+
+console.log(numero === texto);
+
+
+// typeof
+
+console.log(typeof numero);
+console.log(typeof texto);
---
+++ template-strings.js
@@ -0,0 +1,12 @@
+const nome = "Bruno";
+const idade = 21;
+const cidadeNascimento = "Piracicaba";
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} anos, e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao)
+
+
+const d = "";
+
+console.log(d == 0)
